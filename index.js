16bf27b594b5a22b4f4761f315a3a230410b9ced
bascil <basilndonga@gmail.com>
2019-06-08 10:54:45 +0300

    minor changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Joi =require('joi');
 const express = require('express');
 const app  =  express();
-app.use(express.json());
+app.use(express.json()); //set req.body property
 
 const movies = [
 
